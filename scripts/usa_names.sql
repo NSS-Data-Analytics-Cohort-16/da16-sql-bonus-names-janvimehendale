c10222f671ac1030c759e42468e884d1a5a1072e
janvimehendale <janvimehendale@gmail.com>
2025-10-06 18:51:31 -0500

    Working on 14

--- scripts/usa_names.sql
+++ scripts/usa_names.sql
@@ -4,11 +4,13 @@
 SELECT 
 	COUNT(*)
 FROM names;
+
 -------------------------------------------------------------------------------------------
 -- 2.	How many total registered people appear in the dataset? 1957046
 SELECT 
 	COUNT(num_registered) AS total_registered
 FROM names;
+
 --------------------------------------------------------------------------------------------
 -- 3.	Which name had the most appearances in a single year in the dataset?
 --The names appeared twice in the respective years
@@ -20,15 +22,14 @@ GROUP BY name, year
 ORDER BY most_appearance DESC, year;
 
 -------------------------------------------------------------------------------------------
-
 -- 4.	What range of years are included?
 --1880-2018
 SELECT
 	MIN(year) AS min_year,
 	MAX(year) AS max_year
 FROM names;
--------------------------------------------------------------------------------------------
 
+-------------------------------------------------------------------------------------------
 -- 5.	What year has the largest number of registrations?
 -- Year 2008 has the max registrations - 35079
 SELECT
@@ -39,7 +40,6 @@ GROUP BY year
 ORDER BY no_of_registrations DESC;
 
 -------------------------------------------------------------------------------------------
-
 -- 6.	How many different (distinct) names are contained in the dataset? - 98400
 SELECT
 	COUNT(DISTINCT name)
@@ -62,28 +62,20 @@ GROUP BY gender
 ORDER BY COUNT(*) DESC;
 
 -------------------------------------------------------------------------------------------
-
 -- 8.	What are the most popular male and female names overall (i.e., the most total registrations)?
---481 names with 139 registration each - Male
---446 names with 139 registration each - Female
+-- multiple names with 139 registration each - Male
+--multiple names with 139 registration each - Female
 SELECT 
-	name, 
-	COUNT(*) As most_popular_name,
-	gender
+	name AS most_popular_name, 
+	gender,
+	count(num_registered) AS total_registrations
 FROM names
-GROUP BY name, gender
-ORDER BY most_popular_name DESC;
-
---OR-- both have different results
-
-SELECT name, COUNT(*)
-FROM names
-GROUP BY name
-ORDER BY COUNT(*) DESC;
+GROUP BY gender, name
+ORDER BY total_registrations DESC;
 
 -------------------------------------------------------------------------------------------
 -- 9.	What are the most popular boy and girl names of the first decade of the 2000s (2000 - 2009)?
-F = Darrah, M = Jamarian
+F = Darrah, M = Jamarian, multiple entries with count =10
 SELECT 
 	name, 
 	COUNT(*),
@@ -94,7 +86,6 @@ GROUP BY name, gender
 ORDER BY COUNT(*) DESC;
 
 -------------------------------------------------------------------------------------------
-
 -- 10.	Which year had the most variety in names (i.e. had the most distinct names)?
 --Year 2008
 SELECT 
@@ -105,8 +96,7 @@ GROUP BY year
 ORDER BY variety DESC;
 
 -------------------------------------------------------------------------------------------
-
--- 11.	What is the most popular name for a girl that starts with the letter X? - Xenia
+-- 11.	What is the most popular name for a girl that starts with the letter X? - Xenia, 97 count
 SELECT
 	name,
 	COUNT(*) AS count
@@ -117,9 +107,7 @@ gender = 'F'
 GROUP BY name
 ORDER BY count DESC;
 
-
 -------------------------------------------------------------------------------------------
-
 -- 12.	How many distinct names appear that start with a 'Q', but whose second letter is not 'u - 46
 SELECT
 	DISTINCT name,
@@ -131,7 +119,6 @@ GROUP BY name
 ORDER BY count DESC;
 
 -------------------------------------------------------------------------------------------
-
 -- 13.	Which is the more popular spelling between "Stephen" and "Steven"? 
 --Use a single query to answer this question. - Stephen with 230 count.
 SELECT
@@ -144,28 +131,39 @@ name = 'Steven'
 GROUP BY name
 ORDER BY most_popuplar_spelling DESC;
 	
-
 -------------------------------------------------------------------------------------------
-
 -- 14.	What percentage of names are "unisex" - that is what percentage of names have been used
---both for boys and for girls?
+--both for boys and for girls?--- Need help
 SELECT
-	COUNT(*),
-	name
-	--COUNT(name)*100/COUNT(name)
+	DISTINCT name AS unisex_name,
+	COUNT(DISTINCT gender) AS gender_count,
+	(COUNT(DISTINCT name)*100/COUNT(name)) AS percentage
 FROM names
-WHERE 
-gender = 'M'
-AND 
-gender = 'F'
-GROUP BY name;
--------------------------------------------------------------------------------------------
+GROUP BY name
+HAVING COUNT(DISTINCT gender) = 2
+ORDER BY percentage DESC;
+
+------------------------------------------------------------------------------------------
 
 -- 15.	How many names have made an appearance in every single year since 1880?
-
+SELECT
+	DISTINCT year,
+	COUNT(name)
+FROM names
+WHERE 
+	year BETWEEN 1880 AND 2018
+GROUP BY name, year
+ORDER BY year;
 -------------------------------------------------------------------------------------------
 
 -- 16.	How many names have only appeared in one year?
+SELECT 
+	name, 
+	year
+FROM names
+WHERE  
+
+
 
 -------------------------------------------------------------------------------------------
 
@@ -181,4 +179,5 @@ GROUP BY name;
 
 -------------------------------------------------------------------------------------------
 
--- 20.	Come up with a question that you would like to answer using this dataset. Then write a query to answer this question.
+-- 20.	Come up with a question that you would like to answer using this dataset.
+--Then write a query to answer this question.
